d0aee6c345c8510652376d6cf069f1312e7cd6e3
Darkdriller <83203139+Darkdriller@users.noreply.github.com>
2021-05-11 00:47:21 +0530

    Update decoder.py

--- decoder.py
+++ decoder.py
@@ -1,11 +1,6 @@
-import base64
-flag=open("cipher.txt","r").read()
-while "inctfj" not in str(flag):
-    try:
-        flag=base64.b16decode(flag)
-    except:
-        try:
-            flag=base64.b32decode(flag)
-        except:
-            flag=base64.b64decode(flag)
+flag="" #use python2 .decode not avalable in python3
+for i in range(5):
+	flag=flag.decode('hex')
+	flag= flag.decode('base64')
+	
 print(flag)
